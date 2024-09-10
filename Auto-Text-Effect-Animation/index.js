f1d55c6426bb5cf232854c9cb4fd87043fa51fc5
bilyana-ancheva <bilyana.ancheva@bootcamp-upgrade.com>
2024-09-10 13:14:44 +0200

    Auto text project

---
+++ Auto-Text-Effect-Animation/index.js
@@ -0,0 +1,39 @@
+const containerEl = document.querySelector(".container");
+
+const careers = [
+  "Salesforce Marketing Cloud Specialist",
+  "Web and Email Developer",
+  "working with Javascript, HTML, CSS",
+  "Open to work",
+  "..."
+];
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+
+    // Reset careerIndex if it reaches the end of the careers array
+    if (careerIndex === careers.length) {
+      careerIndex = 0;
+    }
+
+    // Set a longer timeout before starting the next career
+    setTimeout(updateText, 1000); // 1 second pause between careers
+  } else {
+    // Regular update speed while rendering the text
+    setTimeout(updateText, 100);
+  }
+}
